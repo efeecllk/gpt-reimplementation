d4a6f88a4af80782ac07e633ca7fb4e9d65ba64e
Efe Çelik <efecelik576@gmail.com>
2025-03-28 16:43:38 +0300

    class MLP

--- gpt_reimp_EFE/train_gpt2.py
+++ gpt_reimp_EFE/train_gpt2.py
@@ -4,6 +4,20 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, config.n_embd * 4)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
 class Block(nn.Module):
     def __init__(self,config):
         super().__init__()
