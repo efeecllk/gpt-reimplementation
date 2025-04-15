173b3bf3b80f2b2151cec21fbbe874096aa59471
Efe Çelik <efecelik576@gmail.com>
2025-04-15 12:12:52 +0300

    Update train_gpt2.py

--- gpt_reimp_EFE/train_gpt2.py
+++ gpt_reimp_EFE/train_gpt2.py
@@ -141,9 +141,49 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+#
+
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+
+#
 
 
-
-model = GPT.from_pretrained('gpt2')
+num_return_sequences = 5
+max_length = 30
+model = GPT(GPTConfig())
+model.eval()
 print('it works')
 print(model)
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+
+
+device = torch.device(device)
+x = tokens.to(device)
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits, _= model(x)
+        logits = logits[:, -1, :]
+        probs = F.softmax(logits, dim=1)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+        x = torch.cat((x, xcol), dim=1)
+
+for i in range(num_return_sequences):
+    tokens_list = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens_list)
+    print(">", decoded)
